e7cf44dae2419498ca9ea4e61f63ca6cef5c6f8e
jemaydup <juanemilio.maydup@gmail.com>
2021-11-19 00:31:55 -0300

    class grupo

--- class/grupo.php
+++ class/grupo.php
@@ -1,23 +1,18 @@
 <?php
     class GdA_Grupo{
-
         // Connection
         private $conn;
-
         // Table
         private $db_table = "GdA_Grupo";
-
         // Columns
         public $IDGrupo;
         public $Nombre_grupo;
         public $RP;
         public $Activo;
-       
         // Db connection
         public function __construct($db){
             $this->conn = $db;
         }
-
         // GET ALL
         public function getGdA_Grupos(){
             $sqlQuery = "SELECT IDGrupo, Nombre_grupo, RP   FROM " . $this->db_table . " WHERE Activo = '1'";
@@ -25,7 +20,6 @@
             $stmt->execute();
             return $stmt;
         }
-
         // CREATE
         public function createGdA_Grupo(){
             $sqlQuery = "INSERT INTO". $this->db_table ."
@@ -34,29 +28,22 @@
                         RP = :RP, 
                         Activo = :Activo, 
                         IDGrupo = :IDGrupo";
-        
-            $stmt = $this->conn->prepare($sqlQuery);
-        
+             $stmt = $this->conn->prepare($sqlQuery);
             // sanitize
             $this->Nombre_grupo=htmlspecialchars(strip_tags($this->Nombre_grupo));
             $this->RP=htmlspecialchars(strip_tags($this->RP));
             $this->Activo=htmlspecialchars(strip_tags($this->Activo));
             $this->IDGrupo=htmlspecialchars(strip_tags($this->IDGrupo));
-  
-        
             // bind data
             $stmt->bindParam(":Nombre_grupo", $this->Nombre_grupo);
             $stmt->bindParam(":RP", $this->RP);
             $stmt->bindParam(":Activo", $this->Activo);
             $stmt->bindParam(":IDGrupo", $this->IDGrupo);
-      
-        
             if($stmt->execute()){
                return true;
             }
             return false;
         }
-
         // UPDATE
         public function getSingleGdA_Grupo(){
             $sqlQuery = "SELECT
@@ -68,22 +55,15 @@
                     WHERE Activo = '1' and 
                        IDGrupo = ? 
                     LIMIT 0,1";
-
             $stmt = $this->conn->prepare($sqlQuery);
-
             $stmt->bindParam(1, $this->IDGrupo);
-
             $stmt->execute();
-
             $dataRow = $stmt->fetch(PDO::FETCH_ASSOC);
-            
             $this->Nombre_grupo = $dataRow['Nombre_grupo'];
             $this->RP = $dataRow['RP'];
             $this->Activo = $dataRow['Activo'];
             $this->IDGrupo = $dataRow['IDGrupo'];
-       
         }        
-
         // UPDATE
         public function updateGdA_Grupo(){
             $sqlQuery = "UPDATE
@@ -95,20 +75,16 @@
                         
                     WHERE 
                         IDGrupo = :IDGrupo";
-        
             $stmt = $this->conn->prepare($sqlQuery);
-        
             $this->Nombre_grupo=htmlspecialchars(strip_tags($this->Nombre_grupo));
             $this->RP=htmlspecialchars(strip_tags($this->RP));
             $this->Activo=htmlspecialchars(strip_tags($this->Activo));
             $this->IDGrupo=htmlspecialchars(strip_tags($this->IDGrupo));
-        
             // bind data
             $stmt->bindParam(":Nombre_grupo", $this->Nombre_grupo);
             $stmt->bindParam(":RP", $this->RP);
             $stmt->bindParam(":Activo", $this->Activo);
             $stmt->bindParam(":IDGrupo", $this->IDGrupo);
-        
             if($stmt->execute()){
                return true;
             }
@@ -119,17 +95,12 @@
         function deleteGdA_Grupo(){
             $sqlQuery = "DELETE FROM " . $this->db_table . " WHERE IDGrupo = ?";
             $stmt = $this->conn->prepare($sqlQuery);
-        
             $this->IDGrupo=htmlspecialchars(strip_tags($this->IDGrupo));
-        
             $stmt->bindParam(1, $this->IDGrupo);
-        
             if($stmt->execute()){
                 return true;
             }
             return false;
         }
-
     }
 ?>
-
